56dfc57420c23fb69c1e8790a4e1eac1b032d434
Xwar5 <za2ha2r8@gmail.com>
2023-09-30 13:10:26 +0300

    add detail page for posts

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -12,3 +13,5 @@ class Post(models.Model):
     datetime = models.DateTimeField()
     category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default="no category")
     
+    def get_absolute_url(self):
+        return reverse("post-details", args=[str(self.id)])
