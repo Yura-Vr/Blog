b7fee982daa4fa4ba90bac2b700e70af8c763732
Xwar5 <za2ha2r8@gmail.com>
2023-10-28 13:32:11 +0300

    add cat amd auth links

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -7,5 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", view=views.index, name="home"),
-    path("<int:pk>", view=views.PostDetailView.as_view(), name="post-details")
+    path("<int:pk>", view=views.PostDetailView.as_view(), name="post-details"),
+    path("category", view=views.CategoryList.as_view(), name="category_list"),
+    path("author", view=views.AuthorList.as_view(), name="author_list"),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
